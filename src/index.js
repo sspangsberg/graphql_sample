1cffb022c10faa4873d22d84124b5897adb9d459
sspangsberg <smsj@easv.dk>
2021-03-02 15:03:38 +0100

    Added .env file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { typeDefs } from './typeDefs';
 
 const mongoose = require('mongoose');
 
+//load configuration from .env file
+require('dotenv-flow').config();
+
 const startServer = async () => {
     const app = new express();    
     
@@ -15,7 +18,7 @@ const startServer = async () => {
 
     server.applyMiddleware({ app });
 
-    await mongoose.connect('mongodb+srv://dbuser:1234@cluster0.wmgzy.mongodb.net/graphql?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect(process.env.DBHOST, {useNewUrlParser: true, useUnifiedTopology: true});
     
     app.listen({ port: 4000}, () =>
         console.log(`Server ready at http://localhost:4000${ server.graphqlPath }`)
